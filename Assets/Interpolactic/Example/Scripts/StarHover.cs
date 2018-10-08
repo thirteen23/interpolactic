61296677b8985de9c1891313ee28dbe37ff8b74d
Max Wade <max.wade@thirteen23.com>
2018-10-08 15:47:57 -0500

    Added example of ping-pong to demo scene

---
+++ Assets/Interpolactic/Example/Scripts/StarHover.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Interpolactic;
+
+public class StarHover : MonoBehaviour {
+
+    public float distance;
+    public float duration;
+
+	void Start () {
+        Vector3 initalPosition = transform.position + transform.up * distance / 2;
+        Vector3 driftTransformation = -transform.up * distance;
+
+        new Interpolation(t => transform.position = initalPosition + driftTransformation * t)
+            .Duration(duration)
+            .EasingFunction(Mathf.SmoothStep)
+            .Repeats(true)
+            .PingPong(true)
+            .Build(this)
+            .Play();
+	}
+}
